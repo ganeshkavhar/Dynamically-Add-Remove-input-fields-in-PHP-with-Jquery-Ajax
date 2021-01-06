5ff9201e2f7db01f46727654ca99cdcccc46e62f
ganesh kavhar <kavharg@gmail.com>
2021-01-06 06:15:40 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+How to run the Dynamically Add / Remove input fields in PHP with Jquery Ajax
+
+1.Download the zip file
+
+2.Extract the file and copy "dynamically-add-remove-field" folder
+
+3.Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+
+4.Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+5.Create a database with name practice
+
+6.Import tblskills.sql file(given inside the zip package in SQL file folder)
+
+7.Run the script http://localhost/dynamically-add-remove-field
---
+++ tblskills.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2020 at 08:28 AM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `practice`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblskills`
+--
+
+CREATE TABLE `tblskills` (
+  `id` int(11) NOT NULL,
+  `skill` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblskills`
+--
+
+INSERT INTO `tblskills` (`id`, `skill`) VALUES
+(1, 'PHP'),
+(2, 'MySQL'),
+(3, 'PHP'),
+(4, 'MySQL'),
+(5, 'PHP');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblskills`
+--
+ALTER TABLE `tblskills`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblskills`
+--
+ALTER TABLE `tblskills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
