acbcdfff5f41c7e89e6c3154337c9e86b2a4d0db
ganesh kavhar <kavharg@gmail.com>
2021-01-06 06:16:49 +0530

    Update index.php

--- dynamically-add-remove-field/index.php
+++ dynamically-add-remove-field/index.php
@@ -27,7 +27,7 @@ echo "<script>alert('Please enter skill');</script>";
 ?>
 <html>
 	<head>
-		<title>PHPGurukul Programmin Blog | Dynamically Add or Remove input fields in PHP with JQuery</title>
+		<title> Programmin Blog | Dynamically Add or Remove input fields in PHP with JQuery</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
